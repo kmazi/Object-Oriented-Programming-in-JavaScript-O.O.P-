8fd4b37e3167bb12e50a7db0f0590f5e07de95ce
kmazi <kingsleyu13@yahoo.com>
2017-04-10 16:50:15 +0100

    The class structure without working function

---
+++ OOP.js
@@ -0,0 +1,35 @@
+﻿class BankAccount{
+    constructor(accName, accNo, accBalance){
+        this._accName = accName;
+        this._accNo = accNo;
+        this._accBalance = accBalance;
+    }
+  
+    getAccNO(){
+        return 'Your account number is '+ this._accNo;
+    }
+  
+    getAccBalance(){
+        return 'Your account number is '+ this.accBalance;
+    }
+}
+
+class SavingsAccount extends BankAccount{
+    constructor(accName, accNo, accBalance){
+        new BankAccount(accName, accNo, accBalance);
+        this.minDeposit = 1000;
+    }
+  
+    this.deposit = function(){
+    
+    }
+}
+
+class CurrentAccount extends BankAccount{
+    constructor(accName, accNo, accBalance){
+        new BankAccount(accName, accNo, accBalance);
+        this.minDeposit = 5000;}
+}
+
+let acc = new BankAccount("Mazi", "0084453675",40000);
+let currAcc = new BankAccount("Mazi", "0084253675",40000);
