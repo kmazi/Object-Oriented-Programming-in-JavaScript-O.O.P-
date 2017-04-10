76544594a29c6aa44ed8d0f6a44b5fb165964254
kmazi <kingsleyu13@yahoo.com>
2017-04-10 16:57:32 +0100

    Just change something in my function not previously working

--- OOP.js
+++ OOP.js
@@ -1,35 +1,42 @@
 ﻿class BankAccount{
-    constructor(accName, accNo, accBalance){
-        this._accName = accName;
-        this._accNo = accNo;
-        this._accBalance = accBalance;
-    }
+  constructor(accName, accNo, accBalance){
+this._accName = accName;
+this._accNo = accNo;
+this._accBalance = accBalance;
+  }
   
-    getAccNO(){
-        return 'Your account number is '+ this._accNo;
-    }
+  getAccNO(){
+    return 'Your account number is '+ this._accNo;
+  }
   
-    getAccBalance(){
-        return 'Your account number is '+ this.accBalance;
-    }
+   getAccBalance(){
+    return 'Your account number is '+ this.accBalance;
+  }
 }
 
 class SavingsAccount extends BankAccount{
-    constructor(accName, accNo, accBalance){
-        new BankAccount(accName, accNo, accBalance);
-        this.minDeposit = 1000;
-    }
+  constructor(accName, accNo, accBalance){
+    new BankAccount(accName, accNo, accBalance);
+    this.minDeposit = 1000;
+  }
   
-    this.deposit = function(){
-    
+  function deposit(amt){
+    if(amt<=0){
+      return "You have not increased your account balance";
     }
+    else{
+      this.accBalance += amt;
+      return "You have successfully deposited "+ this.accBalance;
+    }
+  }
 }
 
 class CurrentAccount extends BankAccount{
     constructor(accName, accNo, accBalance){
-        new BankAccount(accName, accNo, accBalance);
-        this.minDeposit = 5000;}
+    new BankAccount(accName, accNo, accBalance);
+    this.minDeposit = 5000;}
 }
 
-let acc = new BankAccount("Mazi", "0084453675",40000);
+let acc = new SavingsAccount("Mazi", "0084453675",40000);
+console.log(acc.deposit(56000));
 let currAcc = new BankAccount("Mazi", "0084253675",40000);
