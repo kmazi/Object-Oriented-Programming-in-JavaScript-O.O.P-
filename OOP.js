c24d3175876de4f12eb2ce2461bf23a7ef6fc751
Touchstone <kingsleyu13@yahoo.com>
2017-04-11 06:35:13 +0100

    Just corrected the bug in my inherited class

--- OOP.js
+++ OOP.js
@@ -4,39 +4,38 @@ this._accName = accName;
 this._accNo = accNo;
 this._accBalance = accBalance;
   }
+   getAccBalance(){
+    return 'Your account number is '+ this._accBalance;
+  }
   
   getAccNO(){
     return 'Your account number is '+ this._accNo;
   }
   
-   getAccBalance(){
-    return 'Your account number is '+ this.accBalance;
-  }
 }
 
 class SavingsAccount extends BankAccount{
   constructor(accName, accNo, accBalance){
-    new BankAccount(accName, accNo, accBalance);
-    this.minDeposit = 1000;
+    super (accName, accNo, accBalance);
+  this.minDeposit = 1000;
   }
-  
-  function deposit(amt){
+  deposit(amt){
     if(amt<=0){
       return "You have not increased your account balance";
     }
     else{
-      this.accBalance += amt;
-      return "You have successfully deposited "+ this.accBalance;
+      this._accBalance += amt;
+      return "You have successfully deposited "+ this._accBalance;
     }
   }
 }
 
 class CurrentAccount extends BankAccount{
     constructor(accName, accNo, accBalance){
-    new BankAccount(accName, accNo, accBalance);
     this.minDeposit = 5000;}
 }
 
-let acc = new SavingsAccount("Mazi", "0084453675",40000);
-console.log(acc.deposit(56000));
 let currAcc = new BankAccount("Mazi", "0084253675",40000);
+let savAcc = new SavingsAccount("Mazi", "0084253675",40000);
+console.log(savAcc.deposit(4000));
+
